bbaa924a7275396a502b951c4ad579bfe69c8e9b
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-03-11 02:55:18 +0200

    cleaned cors lines and added deploy:full script

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require("node:path");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 morgan.token("post-data", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
